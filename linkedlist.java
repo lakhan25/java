43f3a6b9fc77278a17277de0e53b7bda0d140a59
keshavsingh4522 <keshavsingh4522@gmail.com>
2019-10-02 23:24:45 +0530

    adding in linkedlist

---
+++ linkedlist.java
@@ -0,0 +1,12 @@
+import java.util.LinkedList;
+class Main {
+    public static void main(String[] args){
+        LinkedList<String> animals = new LinkedList<>();
+        // Add elements to LinkedList
+        animals.add("Dog");
+        animals.add("Cat");
+        animals.add("Horse");
+        System.out.println("LinkedList: " + animals);
+    }
+}
+
